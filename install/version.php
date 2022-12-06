98edfa1e4717a1cdbfc7f5a7576ae6e089b02ebe
MaximKad <max.kad@emisart.ru>
2022-12-06 10:35:54 +0300

    Update version.php

--- install/version.php
+++ install/version.php
@@ -1,5 +1,5 @@
 <?php
 $arModuleVersion = array(
-    "VERSION"        => "Beta 1.0.1",
-    "VERSION_DATE" => "2022-10-31 17:02:00"
+    "VERSION"        => "Beta 1.0.2",
+    "VERSION_DATE" => "2022-12-06 10:32:00"
 );
